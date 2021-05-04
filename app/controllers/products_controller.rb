a94670594205fe654179ad9308a2f719cab168af
spirit-8 <mijukumononinja@ezweb.ne.jp>
2021-05-04 21:08:54 +0900

    修正１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    # @products = []
     @products = Product.includes(:user).order("created_at DESC")
   end
   def new
